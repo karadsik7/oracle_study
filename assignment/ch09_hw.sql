3abd6f1473b46faf12a1754751886cb941368910
me <>
2018-04-06 11:41:15 +0900

    오라클

--- assignment/ch09_hw.sql
+++ assignment/ch09_hw.sql
@@ -1,6 +1,8 @@
 --사원명, 사원이 속한 부서명, 부서가 속한 도시명을 출력하되 조인을 사용하지 말고 subquery를 사용해서 출력하시오.
 
-select first_name, department_name, city from employees, (select * from departments), (select * from locations);
+select first_name, (select department_name from departments where department_id = e.department_id),
+(select city from locations l, departments d where l.location_id = d.location_id and e.department_id = d.department_id) 
+from employees e;
 
 --다음 두 쿼리는 같은 데이터를 출력한다. 두 쿼리를 비교할 때 어느 쿼리가 성능면에서 효율적인지를 판단하고 그 이유를 서술하세요.
 
@@ -17,20 +19,18 @@ select e.first_name, d.department_name from employees e, (select * from departme
 
 
 --사원테이블에서 부서별 급여가 가장 높은 사원의 이름과 급여와 부서아이디를 출력하세요.
+
+--****사원을 출력하는데 salary가 자기가 속한 부서의 최대값과 일치********
+
 select department_id, max(salary) from employees group by department_id;
-
-select first_name, salary, department_id from employees 
-    where salary in((select max(salary) from employees group by department_id));
+select first_name, salary from employees where salary = (자신이 속한 부서의 최대 월급);
+select first_name, salary, department_id from employees e 
+    where salary = (select max(salary) from employees where department_id = e.department_id) order by salary desc;
     
-
-select first_name, salary, department_id from (select max(salary) from employees group by department_id);
-select * from jobs;
-select * from employees;
---사원을 출력하는데 salary가 자기가 속한 부서의 최대값과 일치
-select first_name, salary, department_id from employees 
-where (select max(salary), department_id from employees group by department_id);
-
-
+    
+--스티븐과 같은 부서에서 일하는 사람 출력
+select *from employees where first_name = 'Steven';
+select * from employees where department_id in(select department_id from employees where first_name = 'Steven');
 
 --dept테이블은 department_id가 10씩 증가하는 구조이다. 이 규칙을 깨뜨리지 않도록 빈칸을 채우시오. (계속사용이 가능한 쿼리여아만 함)
 --insert into dept values (------------,'research and development', 200, 1500)
